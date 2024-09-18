b09bc206f32c61ce99506ecbab6d6e545ce7fb87
Grant Spence <gspence@redhat.com>
2024-09-18 17:22:51 -0400

    [WILL SQUASH] Refactor IsProxyProtocolNeeded
    IsProxyProtocolNeeded must get the LB Type from the service,
    as the status now may not accurately reflect the actual LB Type
    during an update. However, we can't rely only on the service
    because it doesn’t exist during the initial bootstrap (and potentially
    when it gets deleted). In that case, it's safe to use the status to
    determine the anticipated LB Type.
    
    Without this commit results in a misalignment of the proxy
    protocol value between the load balancer and the router deployment
    when an LB type change is pending.
    
    This commit will be squashed into the previous one, as both are dependent
    on each other. I've kept them separate for easier review.

--- pkg/operator/controller/ingress/status_test.go
+++ pkg/operator/controller/ingress/status_test.go
@@ -3117,7 +3117,7 @@ func Test_computeIngressUpgradeableCondition(t *testing.T) {
 					},
 				},
 			}
-			wantSvc, service, err := desiredLoadBalancerService(ic, deploymentRef, platformStatus, true, true)
+			wantSvc, service, err := desiredLoadBalancerService(ic, deploymentRef, platformStatus, true, true, true)
 			if err != nil {
 				t.Errorf("unexpected error from desiredLoadBalancerService: %v", err)
 				return
@@ -3227,7 +3227,7 @@ func Test_computeIngressEvaluationConditionsDetectedCondition(t *testing.T) {
 				},
 			}
 
-			wantSvc, service, err := desiredLoadBalancerService(ic, deploymentRef, platformStatus, true, true)
+			wantSvc, service, err := desiredLoadBalancerService(ic, deploymentRef, platformStatus, true, true, true)
 			if err != nil {
 				t.Fatalf("unexpected error from desiredLoadBalancerService: %v", err)
 			}
